bbf529aff3c011ce56f045d5ae63baa4a162aac5
Katarzyna Winkler <kasia.winkler99@gmail.com>
2020-11-16 20:36:17 +0100

    01 - test code

--- tests/Zad01Test.py
+++ tests/Zad01Test.py
@@ -2,11 +2,20 @@ from sample.zad01 import RomanNumerals
 import unittest
 
 
+class RomanNumeralsTest(unittest.TestCase):
 
+    def test_from_file(self):
+        fileTest = open("data/zad01.txt")
+        tmpRoman = RomanNumerals()
+        for line in fileTest:
+            if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+                continue
+            else:
+                data = line.split(" ")
+                inp, result = int(data[0]), data[1].strip("\n")
+                self.assertEqual(tmpRoman.roman(inp), result)
+        fileTest.close()
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
